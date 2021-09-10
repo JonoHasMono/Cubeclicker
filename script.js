acae3d35e830b6eabf929cdf8db9a86feda71dea
JonoHasMono <fry22331@lcsd2kids.org>
2021-09-10 08:30:20 -0600

    created the description that appears and disappears for the first upgrade button, I plan to work on the actual fuctionality and cost of the upgrade next

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let versionNum = "0.0.3";
+let versionNum = "0.0.4";
 let score = 0
+let upOneOpen = false
 
 function startGame() {
     const bodyVar = document.createElement("div");
@@ -35,9 +36,65 @@ function startGame() {
     scoreBottom.innerHTML = "(Click the Jerma cube to get more)";
     bodyVar.appendChild(scoreBottom);
 
+    let upOneDesc = document.createElement("div");
+            upOneDesc.classList.add("up1D");
+            upOneDesc.innerHTML = "Increases Jerma's size, granting more Jerma bucks per click"
+
+    function spawnCubeParticle() {
+        let cubeParticle = document.createElement("div");
+        let particleTop = 47;
+        let particleSpeed = 0
+        cubeParticle.classList.add("cubeParticle");
+        cubeParticle.style.left = (45 + (Math.floor(Math.random() * 11))).toString() + "%"
+        cubeParticle.style.top = particleTop + "%"
+        bodyVar.appendChild(cubeParticle);
+        moveParticle();
+        function moveParticle() {
+            setTimeout(() => {
+                if (particleTop < 100) {
+                particleTop = particleTop + particleSpeed;
+                particleSpeed = particleSpeed + 0.01;
+                cubeParticle.style.top = particleTop + "%";
+                moveParticle()
+                } else {
+                    bodyVar.removeChild(cubeParticle);
+                }
+            }, 10)
+        }
+    }
+
     function jermaClicked() {
+        spawnCubeParticle();
         score = score + 1;
         scoreTop.innerHTML = "You have " + score + " Jerma bucks";
+    }
+
+    function showUpgrades() {
+        function showUpgradeOne() {
+            let upgradeOne = document.createElement("div");
+            upgradeOne.classList.add("up1");
+            upgradeOne.addEventListener("mouseover", openUpOne)
+            bodyVar.appendChild(upgradeOne);
+        }
+        showUpgradeOne();
+    }
+
+    function openUpOne() {
+        if (upOneOpen == false) {
+            upOneOpen = true;
+            bodyVar.appendChild(upOneDesc);
+        }
+    }
+    let upDel = document.createElement("div");
+    upDel.classList.add("upDel");
+    upDel.addEventListener("mouseover", delUpDesc);
+    bodyVar.appendChild(upDel);
+
+    function delUpDesc() {
+        if(upOneOpen == true) {
+            upOneOpen = false;
+            bodyVar.removeChild(upOneDesc);
+        }
     }
 
     document.addEventListener('keydown', logKey);
@@ -51,6 +108,8 @@ function startGame() {
             shooting = true;
           }
     }
+
+    showUpgrades();
 }
 
 
